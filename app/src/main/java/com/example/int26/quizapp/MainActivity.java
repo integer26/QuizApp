df6406e85463741f7bd1206e2b4cf698549438e9
giorgioschirano <giorgio.schirano@gmail.com>
2017-12-28 19:15:27 +0100

    Initial commit

---
+++ app/src/main/java/com/example/int26/quizapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.int26.quizapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
